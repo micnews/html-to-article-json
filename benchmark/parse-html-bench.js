3bde48944345bbc7b487c5f769a0641f5c5f9c1d
David Björklund <david.bjorklund@gmail.com>
2015-09-14 22:31:30 +0200

    Add parse-html benchmark

---
+++ benchmark/parse-html-bench.js
@@ -0,0 +1,18 @@
+'use strict';
+
+require('../test/browser');
+
+var prettyHrtime = require('pretty-hrtime');
+
+var parseHtml = require('../lib/parse-html');
+var fs = require('fs');
+var text = document.createElement('div');
+text.innerHTML = fs.readFileSync(__dirname + '/text.html', 'utf8');
+
+var start = process.hrtime();
+
+for (var i = 0; i < 100; ++i) {
+  parseHtml(text);
+}
+
+console.log('duration:', prettyHrtime(process.hrtime(start)));
