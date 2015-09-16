fcc84928946382146bb2d92be56f29bacae108b9
David Björklund <david.bjorklund@gmail.com>
2015-09-16 11:37:23 +0200

    Update example/client.js

--- example/client.js
+++ example/client.js
@@ -5,18 +5,22 @@ var render = require('../lib/render');
 var patch = require('incremental-dom').patch;
 var Immutable = require('immutable');
 
-var elm = document.body.appendChild(document.createElement('div'));
+var wrapper = document.body.appendChild(document.createElement('div'));
+wrapper.className = 'wrapper';
+var elm = wrapper.appendChild(document.createElement('div'));
 elm.contentEditable = true;
-elm.innerHTML = '<div>WTF?</div>';
+elm.innerHTML = 'beepboop';
 
 patch(elm, function () {
   render(Immutable.fromJS(parseHtml(elm)));
 });
-
-elm.onpaste = function () {
+elm.onkeypress = function () {
   process.nextTick(function () {
-    patch(elm, function () {
-      render(Immutable.fromJS(parseHtml(elm)));
-    });
+    parseHtml(elm);
   });
 };
+
+window.parse = function () {
+  parseHtml(elm);
+};
+window.elm = elm;
