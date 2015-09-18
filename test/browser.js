eeacd94570772463f6feaaa110ae782bb27cc7ff
David Björklund <david.bjorklund@gmail.com>
2015-09-18 21:52:57 +0200

    remove some not needed stubs

--- test/browser.js
+++ test/browser.js
@@ -6,13 +6,5 @@ var extendInline = require('xtend/mutable');
 var document = jsdom.jsdom(undefined, {
   virtualConsole: jsdom.createVirtualConsole().sendTo(console)
 });
-document.createRange = function () {
-  return {
-    setStartBefore: function () {},
-    setEndAfter: function () {},
-    setStart: function () {},
-    setEnd: function () {}
-  };
-};
 var window = document.defaultView;
 extendInline(global, window);
