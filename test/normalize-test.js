925b57b24bcb54bd86510bf18c5b4e66377235ee
David Björklund <david.bjorklund@gmail.com>
2015-09-19 17:17:09 +0200

    normalize: remove extra <br>s

--- test/normalize-test.js
+++ test/normalize-test.js
@@ -50,3 +50,109 @@ test('normalize() minimum content with selection markers', function (t) {
 
   t.end();
 });
+
+test('normalize() remove not needed linebreaks', function (t) {
+  var tree = [{
+    type: 'paragraph',
+    children: [
+      { type: 'text', content: 'beep boop' },
+      { type: 'linebreak' }
+    ]
+  }];
+  var tree2 = [{
+    type: 'paragraph',
+    children: [
+      { type: 'text', content: 'beep boop' },
+      { type: 'selection-marker', start: true },
+      { type: 'selection-marker', end: true },
+      { type: 'linebreak' }
+    ]
+  }];
+  var expected = [{
+    type: 'paragraph',
+    children: [
+      { type: 'text', content: 'beep boop' }
+    ]
+  }];
+  var expected2 = [{
+    type: 'paragraph',
+    children: [
+      { type: 'text', content: 'beep boop' },
+      { type: 'selection-marker', start: true },
+      { type: 'selection-marker', end: true }
+    ]
+  }];
+
+  t.deepEqual(normalize(tree), expected);
+  t.deepEqual(normalize(tree2), expected2);
+
+  t.end();
+});
+
+test('normalize() keep needed linebreaks', function (t) {
+  var tree = [{
+    type: 'paragraph',
+    children: [
+      { type: 'linebreak' },
+      { type: 'linebreak' }
+    ]
+  }];
+  var tree2 = [{
+    type: 'paragraph',
+    children: [
+      { type: 'selection-marker', start: true },
+      { type: 'selection-marker', end: true },
+      { type: 'linebreak' },
+      { type: 'linebreak' }
+    ]
+  }];
+  var tree3 = [{
+    type: 'paragraph',
+    children: [
+      { type: 'text', content: 'beep boop' },
+      { type: 'linebreak' },
+      { type: 'linebreak' }
+    ]
+  }];
+  var tree4 = [{
+    type: 'paragraph',
+    children: [
+      { type: 'text', content: 'beep' },
+      { type: 'linebreak' },
+      { type: 'text', content: 'boop' }
+    ]
+  }];
+  var tree5 = [{
+    type: 'paragraph',
+    children: [
+      { type: 'selection-marker', start: true },
+      { type: 'selection-marker', end: true },
+      { type: 'linebreak' }
+    ]
+  }];
+  var tree6 = [{
+    type: 'paragraph',
+    children: [
+      { type: 'selection-marker', start: true },
+      { type: 'linebreak' },
+      { type: 'selection-marker', end: true }
+    ]
+  }];
+  var tree7 = [{
+    type: 'paragraph',
+    children: [
+      { type: 'linebreak' },
+      { type: 'selection-marker', start: true },
+      { type: 'selection-marker', end: true }
+    ]
+  }];
+
+  t.deepEqual(normalize(tree), tree);
+  t.deepEqual(normalize(tree2), tree2);
+  t.deepEqual(normalize(tree3), tree3);
+  t.deepEqual(normalize(tree4), tree4);
+  t.deepEqual(normalize(tree5), tree5);
+  t.deepEqual(normalize(tree6), tree6);
+  t.deepEqual(normalize(tree7), tree7);
+  t.end();
+});
