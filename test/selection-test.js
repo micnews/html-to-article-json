0730559a317d00d6c7b8553010604999b356be09
David Björklund <david.bjorklund@gmail.com>
2015-09-22 21:48:03 +0200

    selection-tests (browser only)

---
+++ test/selection-test.js
@@ -0,0 +1,82 @@
+'use strict';
+
+var saveSelection = require('../lib/save-selection');
+var restoreSelection = require('../lib/restore-selection');
+var test = require('tape');
+
+if (!process.browser) {
+  test = function () {};
+}
+
+test('saveSelection() no current selection', function (t) {
+  var elm = document.body.appendChild(document.createElement('div'));
+  elm.contentEditable = 'true';
+  elm.setAttribute('contenteditable', 'true');
+  elm.innerHTML = '<p>Beep</p>';
+
+  saveSelection(elm);
+  t.equal(elm.innerHTML, '<p>Beep</p>', 'unchanged html');
+
+  t.end();
+});
+
+test('saveSelection() selection', function (t) {
+  var elm = document.body.appendChild(document.createElement('div'));
+  var elm2 = document.body.appendChild(document.createElement('div'));
+  elm.innerHTML = '<p>Beep</p>';
+  var text = elm.querySelector('p').childNodes[0];
+
+  var selection = window.getSelection();
+  selection.removeAllRanges();
+  var range = document.createRange();
+  range.setStart(text, 1);
+  range.setEnd(text, 3);
+  selection.addRange(range);
+
+  saveSelection(elm2);
+  t.equal(elm.innerHTML, '<p>Beep</p>',
+    'root doument must include selection');
+
+  saveSelection(elm);
+  t.equal(
+    elm.innerHTML,
+    '<p>B' +
+    '<span class="selection-marker-start"></span>' +
+    'ee' +
+    '<span class="selection-marker-end"></span>p</p>',
+    'added selection markers'
+  );
+
+  t.end();
+});
+
+test('restoreSelection() no markers', function (t) {
+  var elm = document.body.appendChild(document.createElement('div'));
+  elm.innerHTML = '<p>Beep</p>';
+  window.getSelection().removeAllRanges();
+
+  restoreSelection(elm);
+  t.equal(window.getSelection().rangeCount, 0, 'No ranges');
+  t.end();
+});
+
+test('restoreSelection() markers', function (t) {
+  var elm = document.body.appendChild(document.createElement('div'));
+  elm.innerHTML = '<p>B' +
+    '<span class="selection-marker-start"></span>' +
+    'ee' +
+    '<span class="selection-marker-end"></span>p</p>';
+
+  window.getSelection().removeAllRanges();
+
+  restoreSelection(elm);
+  t.equal(window.getSelection().rangeCount, 1, 'One ranges');
+  elm.normalize();
+  var text = elm.querySelector('p').childNodes[0];
+  var range = window.getSelection().getRangeAt(0);
+  t.equal(range.startContainer, text, 'range.startContainer');
+  t.equal(range.startOffset, 1, 'range.startOffset');
+  t.equal(range.endContainer, text, 'range.endContainer');
+  t.equal(range.endOffset, 3, 'range.endOffset');
+  t.end();
+});
