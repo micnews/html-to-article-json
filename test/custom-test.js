25ee756d33ab5966502877df4be6c41e6a1a07ad
David Björklund <david.bjorklund@gmail.com>
2015-10-11 22:06:44 +0200

    make it possible to add & extend rich types

--- test/custom-test.js
+++ test/custom-test.js
@@ -76,6 +76,66 @@ test('custom text formattings: add underline span-type', function (t) {
   t.end();
 });
 
+test('custom rich type', function (t) {
+  var opts = {
+    saveSelection: false,
+    customRichTypes: [{
+      category: 'foo',
+      render: function (obj) {
+        return new VNode('foo', {
+          attributes: {
+            bar: obj.bar
+          }
+        });
+      },
+      parse: function (elm) {
+        var foo = elm.getElementsByTagName('foo')[0];
+        return {
+          type: 'rich',
+          category: 'foo',
+          bar: foo.getAttribute('bar')
+        };
+      }
+    }]
+  };
+  var update = setupUpdate(opts);
+  var elm1 = document.body.appendChild(document.createElement('div'));
+  elm1.innerHTML = '<foo bar="bas"></foo>';
+  update(elm1);
+  var expected = '<figure>' +
+      '<foo bar="bas"></foo>' +
+    '</figure>';
+  t.equal(elm1.innerHTML, expected);
+  t.end();
+});
+
+test('custom rich type that extend existing with special render', function (t) {
+  var opts = {
+    saveSelection: false,
+    customRichTypes: [{
+      category: 'image',
+      render: function (obj) {
+        return new VNode('img', {
+          attributes: {
+            src: obj.src,
+            alt: obj.alt,
+            class: 'custom-image-class'
+          }
+        });
+      }
+      // no parse needed since the existing parse will be used. Pretty neat!
+    }]
+  };
+  var update = setupUpdate(opts);
+  var elm = document.body.appendChild(document.createElement('div'));
+  var img = elm.appendChild(document.createElement('img'));
+  img.setAttribute('src', 'http://example.com/image.jpg');
+  update(elm);
+  t.equal(elm.innerHTML,
+    '<figure><img src="http://example.com/image.jpg" class="custom-image-class"></figure>');
+  t.end();
+});
+
 function createElement (str) {
   var elm = document.createElement('div');
   elm.innerHTML = str;
