189bbdb41ef9ce6e0c91926839d3fbe14300f3e1
David Björklund <david.bjorklund@gmail.com>
2016-01-22 22:39:06 +0100

    get benchmark to work

--- benchmark/node.js
+++ benchmark/node.js
@@ -1,4 +1,19 @@
 'use strict';
 
-require('../test/utils/mock-jsdom-browser');
-require('./browser');
+import setup from '../lib';
+import fs from 'fs';
+
+const parse = setup();
+const text = fs.readFileSync(__dirname + '/text.html', 'utf8');
+
+const start = Date.now();
+let count = 0;
+let duration;
+
+for (; count < 1000; ++count) {
+  parse(text);
+}
+duration = Date.now() - start;
+
+const msg = (duration / count) + ' ms / iteration';
+console.log(msg);
