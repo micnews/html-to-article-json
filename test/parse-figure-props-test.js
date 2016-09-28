78abb8ada81842cc1ade2bbd4a626712c3365fe1
ellell <lisaovermyr@gmail.com>
2016-09-28 16:40:02 +0200

    Add option for parsing figureProps

---
+++ test/parse-figure-props-test.js
@@ -0,0 +1,52 @@
+import test from './tape-wrapper';
+import setupParse from '../lib';
+import tsml from 'tsml';
+
+test('parse() parseFigureProps, no match', t => {
+  const opts = {
+    parseFigureProps: ['class', 'id']
+  };
+
+  const parse = setupParse(opts);
+
+  const input = tsml`<figure>
+    <img src="http://example.com/image.jpg"/>
+  </figure>`;
+  const actual = parse(input);
+  const expected = [{
+    type: 'embed',
+    embedType: 'image',
+    caption: [],
+    attribution: [],
+    src: 'http://example.com/image.jpg',
+    width: null,
+    height: null,
+    figureProps: {}
+  }];
+
+  t.same(actual, expected);
+});
+
+test('parse() parseFigureProps, only return specified attribute', t => {
+  const opts = {parseFigureProps: ['class']};
+  const parse = setupParse(opts);
+
+  const input = tsml`<figure class='beep' id='boop'>
+    <img src="http://example.com/image.jpg"/>
+  </figure>`;
+  const actual = parse(input);
+  const expected = [{
+    type: 'embed',
+    embedType: 'image',
+    caption: [],
+    attribution: [],
+    src: 'http://example.com/image.jpg',
+    width: null,
+    height: null,
+    figureProps: {
+      class: 'beep'
+    }
+  }];
+
+  t.same(actual, expected);
+});
